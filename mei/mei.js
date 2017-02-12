8e28ed712b2151bc10267e7ed9f3530cd9996c1f
tzarras <TimKitchen@Tims-MacBook-Pro.local>
2017-02-12 11:27:48 -0500

    yo

--- mei/mei.js
+++ mei/mei.js
@@ -1,78 +1,89 @@
 $(document).ready(function() {
 
-	// nothing to see here, pretty simple //
+// nothing to see here, pretty simple //
 
-	// keeping this just in case //
+// keeping this just in case //
 
-	//var isResizing = false,
-    //lastDownX = 0;
+//var isResizing = false,
+    //lastDownX = 0;
 
-	//$(function () {
-	    //var container = $('.container'),
-	        //left = $('.left'),
-	        //right = $('.right'),
-	        //handle = $('.drag');
+//$(function () {
+    //var container = $('.container'),
+        //left = $('.left'),
+        //right = $('.right'),
+        //handle = $('.drag');
 
-	    //handle.on('mousedown', function (e) {
-	        //isResizing = true;
-	        //lastDownX = e.clientX;
-	    //});
+    //handle.on('mousedown', function (e) {
+        //isResizing = true;
+        //lastDownX = e.clientX;
+    //});
 
-	    //$(document).on('mousemove', function (e) {
-	        // don't do anything if we aren't resizing.
-	        //if (!isResizing) 
-	            //return;
-	        
-	        //var offsetRight = container.width() - (e.clientX - container.offset().left);
+    //$(document).on('mousemove', function (e) {
+        // don't do anything if we aren't resizing.
+        //if (!isResizing) 
+            //return;
+        
+        //var offsetRight = container.width() - (e.clientX - container.offset().left);
 
-	        //left.css('right', offsetRight);
-	        //right.css('width', offsetRight);
-	    //}).on('mouseup', function (e) {
-	        // stop 
-	        //isResizing = false;
-	    //});
-	//});
+        //left.css('right', offsetRight);
+        //right.css('width', offsetRight);
+    //}).on('mouseup', function (e) {
+        // stop 
+        //isResizing = false;
+    //});
+//});
 
+    // changes
+    //var currentlySelectedBox;
+    //var setHeight = function () {
+        //if (currentlySelectedBox) {
+            //var h = currentlySelectedBox.height()+30;
+            //currentlySelectedBox.css("height", h);
+        //}
+    //}
 
+$(".box",this).click(function() {
 
-	$(".box",this).click(function() {
-
-		// height of specific info //
-
+// height of this div info //
 		var h = $(".titles",this).height()+30;
 		var self = $(this);
+		var a= $(".archive").height();
 
-		$(this).css("height", h);
-		$(".box").not(this).css("height","88px");
+		$(this).css("height",h).addClass("active");
+		//$(".box").not(this).css({"height": "88px"});
 		$('.images,.iframe-c').hide();
-        $('.images[rel=div' + self.attr('target') +'],.iframe-c[rel=div' + self.attr('target') +']').fadeIn(200);
-        $(".imagesection").scrollTop(0);
+        $('.images[rel=div' + self.attr('target') +'],.iframe-c[rel=div' + self.attr('target') +']').fadeIn(200);
+        $(".imagesection").scrollTop(0);
+		
+		setTimeout(function() {
+			$('.archive').animate({ scrollTop: $('.active').offset().top+h}, 300);
+		}, 60);
 
+});
 
-	});
+$(".bigimage").click(function(){
+         //changes
+$(".text").css({ "background-color":"#01ff70","width":"50px"});
+$(".bigimage").css({ "background-color":"black","width":"20px"});
+$(".title,.year").css("font-size","34");
+$(".info,a").css({"font-size":"34"});
+$(".left").css("width","80%");
+$(".right").css("width","20%");
+$(".iframe-c").css("margin-top","30%");
 
-	$(".bigimage").click(function(){
-		$(".text").css({ "background-color":"#01ff70","width":"50px"});
-		$(".bigimage").css({ "background-color":"black","width":"20px"});
-		$(".title,.year").css("font-size","34");
-		$(".info,a").css({"font-size":"34"});
-		$(".left").css("width","80%");
-		$(".right").css("width","20%");
-		$(".iframe-c").css("margin-top","30%");
+});
 
-	});
+$(".text").click(function(){
+$(".tip").hide();
+$(".text").css({ "background-color":"black","width":"20px"});
+$(".bigimage").css({"background-color":"#01ff70","width":"50px"});
+$(".info,a").css({"font-size":"18"});
+$(".title,.year").css("font-size","34");
+$(".left").css("width","25%");
+$(".right").css("width","75%");
+$(".iframe-c").css("margin-top","10%");
 
-	$(".text").click(function(){
-		$(".tip").hide();
-		$(".text").css({ "background-color":"black","width":"20px"});
-		$(".bigimage").css({"background-color":"#01ff70","width":"50px"});
-		$(".info,a").css({"font-size":"18"});
-		$(".title,.year").css("font-size","34");
-		$(".left").css("width","25%");
-		$(".right").css("width","75%");
-		$(".iframe-c").css("margin-top","10%");
-
-	});
+});
 
 
 
